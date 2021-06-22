337110ed63fa0e27a9be27f6ae8154d854c332d7
2bleO <Onesis.olea@gmail.com>
2021-06-22 11:22:06 -0400

    Update mobileMenu.js
    Fix JS syntax errors

--- js/mobileMenu.js
+++ js/mobileMenu.js
@@ -1,55 +1,55 @@
-var body = document.getElementById("body");
-var menu = document.getElementById("menu");
-var menuicon = document.getElementsByTagName("i")[0];
-var devname = document.getElementById("devname");
-var portfolio = document.getElementsByTagName("li")[0];
-var about = document.getElementsByTagName("li")[1];
-var contact = document.getElementsByTagName("li")[2];
-var envelope = document.getElementsByTagName("li")[3];
+let body = document.getElementById('body');
+let menu = document.getElementById('menu');
+let menuicon = document.getElementsByTagName('i')[0];
+let devname = document.getElementById('devname');
+let portfolio = document.getElementsByTagName("li")[0];
+let about = document.getElementsByTagName('li')[1];
+let contact = document.getElementsByTagName('li')[2];
+let envelope = document.getElementsByTagName('li')[3];
 
 function mobilemenu() {
 
-  menuicon.classList.toggle("fa-bars");
-  menuicon.classList.toggle("fa-times");
-  devname.classList.toggle("name");
-  devname.classList.toggle("name-hide");
-  menu.classList.toggle("desktop-menu");
-  menu.classList.toggle("mobile-menu");
-  body.classList.toggle("scroll");
-  body.classList.toggle("noscroll");
-  envelope.classList.toggle("fa-envelope");
-  envelope.classList.toggle("hide");
+  menuicon.classList.toggle('fa-bars');
+  menuicon.classList.toggle('fa-times');
+  devname.classList.toggle('name');
+  devname.classList.toggle('name-hide');
+  menu.classList.toggle('desktop-menu');
+  menu.classList.toggle('mobile-menu');
+  body.classList.toggle('scroll');
+  body.classList.toggle('noscroll');
+  envelope.classList.toggle('fa-envelope');
+  envelope.classList.toggle('hide');
 }
 
 function link() {
-  if (menu.className == "mobile-menu") {
-    menu.className = "desktop-menu";
+  if (menu.className === 'mobile-menu') {
+    menu.className = 'desktop-menu';
   } else {
-    menu.className = "desktop-menu";
+    menu.className = 'desktop-menu';
   }
-  if (menuicon.className == "fa fa-times") {
-    menuicon.className = "fa fa-bars";
+  if (menuicon.className === 'fa fa-times') {
+    menuicon.className = 'fa fa-bars";
   } else {
-    menuicon.className = "fa fa-bars";
+    menuicon.className = 'fa fa-bars';
   }
-  if (devname.className == "name-hide") {
-    devname.className = "name";
+  if (devname.className === 'name-hide') {
+    devname.className = 'name';
   } else {
-    devname.className = "name";
+    devname.className = 'name';
   }
-  if (body.className == "noscroll") {
-    body.className = "scroll";
+  if (body.className === 'noscroll') {
+    body.className = 'scroll';
   } else {
-    body.className = "scroll";
+    body.className = 'scroll';
   }
-  if (envelope.className == "hide") {
-    envelope.className = "fa-envelope";
+  if (envelope.className === 'hide') {
+    envelope.className = 'fa-envelope';
   } else {
-    envelope.className = "fa-envelope";
+    envelope.className = 'fa-envelope';
   }
 }
 
-menuicon.addEventListener("click", mobilemenu);
-portfolio.addEventListener("click", link);
-about.addEventListener("click", link);
-contact.addEventListener("click", link);
+menuicon.addEventListener('click', mobilemenu);
+portfolio.addEventListener('click', link);
+about.addEventListener('click', link);
+contact.addEventListener('click', link);
