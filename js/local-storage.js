eae085b6409a1f2afb3cd152e87e746c8578cae1
2bleO <Onesis.olea@gmail.com>
2021-06-25 20:58:56 -0400

    Update local-storage.js

--- js/local-storage.js
+++ js/local-storage.js
@@ -19,14 +19,18 @@ if (window.localStorage) {
 
 const storageMet = {
   retrieve() {
-    formData.storedJson = localStorage.getItem('jsonObj');
-    formData.storedObj = JSON.parse(formData.storedJson);
+    formData.storedJson = localStorage.getItem('portfolio');
+    if (formData.storedJson !== null) {
+      formData.storedObj = JSON.parse(formData.storedJson);
+    }
   },
 
   prePop() {
-    formData.name.setAttribute('value', formData.storedObj.name);
-    formData.email.setAttribute('value', formData.storedObj.email);
-    formData.message.setAttribute('placeholder', formData.storedObj.message);
+    if (formData.storedObj !== null) {
+      formData.name.setAttribute('value', formData.storedObj.name);
+      formData.email.setAttribute('value', formData.storedObj.email);
+      formData.message.setAttribute('placeholder', formData.storedObj.message);
+    }
   },
 
   listen() {
@@ -44,7 +48,7 @@ const storageMet = {
 
     formData.toStore = JSON.stringify(formData.userData);
     if (formData.storedJson !== formData.toStore) {
-      window.localStorage.setItem('jsonObj', formData.toStore);
+      window.localStorage.setItem('portfolio', formData.toStore);
     }
   },
 };
